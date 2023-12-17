3eb9f0576516ea773694a3be5331794cf8ef8b79
stevengeorge128 <stevengeorge@arizona.edu>
2023-12-17 15:59:51 -0800

    Got search returning a book

---
+++ server/bookFetchHandler.js
@@ -0,0 +1,10 @@
+function parseBookSearch(bookData){
+    console.log("The book data is ", bookData)
+    
+    return true
+
+}
+
+module.exports = {
+    parseBookSearch,
+}
---
+++ server/main.js
@@ -0,0 +1,40 @@
+
+
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+const bookFetchHandler = require('./bookFetchHandler');
+// const parser = require('body-parser');
+// const cookieParser = require('cookie-parser');
+// app.use(parser.json());
+
+const port = 5000;
+
+app.use("", (req, res, next) => {
+    console.log("\n-----------------------------\nThe request is:\t", req.url, "\n-----------------------------\n")
+    next()
+})
+
+
+
+app.get("/get/mountain", async (req, res) => {
+    try {
+    let gettingBook = await fetch("https://openlibrary.org/search.json?title=my+side+of+the+mountain")
+    if (gettingBook.ok) {
+        let data = await gettingBook.json();
+        let handlingFetchData = bookFetchHandler.parseBookSearch(data)
+        res.status(200).end("Book Found")
+      } else {
+        console.error("Error fetching data:", gettingBook.status, gettingBook.statusText);
+      }
+    } catch (err) {
+        console.error(err);
+        res.status(404).end()
+    }
+})
+
+
+
+
+app.listen(port, () =>
+    console.log(`App listening at http://localhost:${port}`))
