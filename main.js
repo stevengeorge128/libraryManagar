c2ac966da76e4710b622881e56fc4548dd62dda2
stevengeorge128 <stevengeorge@arizona.edu>
2023-12-26 14:27:39 -0800

    added schemas.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
+const { tempBookLookup } = require('./schemas');
 const bookFetchHandler = require('./bookFetchHandler');
 const crypto = require("crypto")
 // const parser = require('body-parser');
@@ -10,46 +11,25 @@ const crypto = require("crypto")
 // app.use(parser.json());
 
 
+/*
+        let toHash = pwd + userObjSalt;
+        let h = crypto.createHash('sha3-256');
+        let data = h.update(toHash, 'utf-8');
+        let hashPwd = data.digest('hex');
+*/
+
+
 const port = 5000;
 
+const validSearches = [];
 
 // Establish mongoDB connection
 const db = mongoose.connection
 const mongoDBURL = "mongodb://127.0.0.1/libraryManager";
 mongoose.connect(mongoDBURL);
 db.on("error", () => { console.log("MongoDB Connection Error") })
-var Schema = mongoose.Schema;
 
-// Establish four required schemas
-var tempLookupSchema = new Schema({
-    // title: String,
-    // authors: [],
-    // isbn: String,
-    // coverIdOpenLib: String,
-    // firstPublished: String,
-    // authorKeyOpenLib: [],
-    // subjectsOpenLib: [],
-    // imgSrc: String,
-    // dewey: String,
-    // congress: String,
-    // extendedInfo: String,
-    // allDocs: String
-
-    title: String,
-    authors: [],
-    link: String,
-    isbn: String,
-    firstPublished: String,
-    imageLink: String,
-    imageHtml: String,
-    extendedInfo: String,
-    allDocs: String
-});
-const tempLookup = mongoose.model("tempLookup", tempLookupSchema);
-
-
-
-
+// Log all routes
 app.use("", (req, res, next) => {
     console.log("\n-----------------------------\nThe request is:\t", req.url, "\n-----------------------------\n")
     next()
@@ -64,35 +44,30 @@ app.use(function (req, res, next) {
 // Serve static files
 app.use(express.static('public_html'))
 
-// First route requested when user searches for a book by title
+// Route requested when user searches with only the title field and leaves the author field blank.
+// Gets books by title using bookFetchHandler methods and then saves the search and responds to the 
+// user.
 app.get("/get/title/:title", async (req, res) => {
     try {
         let handlingFetchData = await bookFetchHandler.parseBookTitleSearch(req.params.title)
         let userResponse = handlingFetchData;
-        let temp = new tempLookup(handlingFetchData);
+        let temp = new tempBookLookup(handlingFetchData);
         let savingTemp = await temp.save();
         let id = savingTemp._id.toHexString()
         delete userResponse.extendedInfo;
         delete userResponse.allDocs;
-
-        //console.log(userResponse);
-        // console.log(handlingFetchData)
         res.status(200).json([userResponse, id]);
-        //res.status(200).json(handlingFetchData);
     } catch (err) {
         console.error(err);
         res.status(404).end()
     }
 })
 
-// // First route requested when user searches for a book by author
+// Routes requested when the user just searches by author and leaves the title field blank. Gets
+// all books by that author and responds.
 app.get("/get/author/:author", async (req, res) => {
     try {
         let handlingFetchData = await bookFetchHandler.parseBookAuthorSearch(req.params.author)
-        //console.log(JSON.stringify(handlingFetchData))
-        // console.log(req.params.author)
-        
-        //console.log(handlingFetchData)
         res.status(200).json(handlingFetchData)
     } catch (err) {
         console.error(err);
@@ -100,23 +75,16 @@ app.get("/get/author/:author", async (req, res) => {
     }
 })
 
-
+// Route requested when user has both the title and author fields filled. Book is found and returned
+// to user
 app.get("/get/authAndTitle/:title/:author", async (req, res) => {
     try {
-        let handlingFetchData = await bookFetchHandler.parseTitleAndAuthorSearch(req.params.title, req.params.author)
-        // console.log(req.params.author)
-        
-        //console.log(handlingFetchData)
-
-        
-        let temp = new tempLookup(handlingFetchData);
+        let handlingFetchData = await bookFetchHandler.parseTitleAndAuthorSearch(req.params.title, req.params.author) 
+        let temp = new tempBookLookup(handlingFetchData);
         let savingTemp = await temp.save();
         let id = savingTemp._id.toHexString()
         delete handlingFetchData.extendedInfo;
         delete handlingFetchData.allDocs;
-
-
-
         res.status(200).json([handlingFetchData, id])
     } catch (err) {
         console.error(err);
---
+++ schemas.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+// Establish mongoDB connection
+var Schema = mongoose.Schema;
+
+
+// Establish four required schemas
+var tempBookLookupSchema = new Schema({
+    title: String,
+    authors: [],
+    link: String,
+    isbn: String,
+    firstPublished: String,
+    imageLink: String,
+    imageHtml: String,
+    extendedInfo: String,
+    allDocs: String
+});
+const tempBookLookup = mongoose.model("tempBookLookup", tempBookLookupSchema);
+
+module.exports = {
+    tempBookLookup,
+};
